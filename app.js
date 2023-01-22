646d2aa79186174cdd5c1be261c5a51afd87d531
r1d3rzz <myintthwaykhine127@gmail.com>
2023-01-22 22:44:24 +0630

    generate QR Code is Working !!! (complete)

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
-const wrapper = document.querySelector(".wrapper");
-const genBtn = document.querySelector(".wrapper button");
+const wrapper = document.querySelector(".wrapper"),
+  input = document.querySelector("input"),
+  img = document.querySelector("img"),
+  genBtn = document.querySelector(".wrapper button");
 
 genBtn.addEventListener("click", () => {
-  wrapper.classList.add("active");
+  if (!input.value) return;
+  genBtn.innerText = "Generating QR Code ...";
+  let qr_value = ` https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${input.value}`;
+  img.src = qr_value;
+  img.addEventListener("load", () => {
+    genBtn.innerText = "Generate QR Code";
+    wrapper.classList.add("active");
+  });
 });
+
+input.addEventListener("keyup", () => {
+  if (!input.value);
+  wrapper.classList.remove("active");
+});
+
+// https://goqr.me/api/    (api resource)
